a3dc1133b1148c5fd42d5af4864eec341f86dc85
lvo711717 <lvo711717@gmail.com>
2024-03-26 19:12:23 +0200

    adding mobx to split logic of handlers and states

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,14 +1,13 @@
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from '../../../app/models/activity';
-import { SyntheticEvent, useState } from "react";
+import { Fragment, SyntheticEvent, useState } from "react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props{
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
-export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props){
+
+export default observer(function ActivityList(){
+    const {activityStore} = useStore();
+    const {deleteActivity, activitiesList, loading} = activityStore;
+ 
     const [target, setTarget] = useState('');
 
     function handleActvityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string)
@@ -18,33 +17,35 @@ export default function ActivityList({activities, selectActivity, deleteActivity
     }
 
     return(
-        <Segment>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.data}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
-                                <Button 
-                                    name={activity.id} 
-                                    loading={submitting && target === activity.id} 
-                                    onClick={(e) => handleActvityDelete(e, activity.id)} 
-                                    floated='right' 
-                                    content='Delete' 
-                                    color='red' 
-                                />
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
+        <Fragment>
+                <Segment>
+                    <Item.Group divided>
+                        {activitiesList.map(activity => (
+                            <Item key={activity.id}>
+                                <Item.Content>
+                                    <Item.Header as='a'>{activity.title}</Item.Header>
+                                    <Item.Meta>{activity.data}</Item.Meta>
+                                    <Item.Description>
+                                        <div>{activity.description}</div>
+                                        <div>{activity.city}, {activity.venue}</div>
+                                    </Item.Description>
+                                    <Item.Extra>
+                                        <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                        <Button 
+                                            name={activity.id} 
+                                            loading={loading && target === activity.id} 
+                                            onClick={(e) => handleActvityDelete(e, activity.id)} 
+                                            floated='right' 
+                                            content='Delete' 
+                                            color='red' 
+                                        />
+                                        <Label basic content={activity.category} />
+                                    </Item.Extra>
+                                </Item.Content>
+                            </Item>
+                        ))}
+                    </Item.Group>
+                </Segment>
+        </Fragment>
     )
-}
+})
